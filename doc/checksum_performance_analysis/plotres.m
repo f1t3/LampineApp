69e2fc1a88f11f34bac61867b71e23e729fa43b9
andgset <andgset@gmail.com>
2021-02-17 20:31:15 +0100

    Cont. sim

--- doc/checksum_performance_analysis/plotres.m
+++ doc/checksum_performance_analysis/plotres.m
@@ -5,12 +5,14 @@ EsN0start = 5;
 EsN0stop  = 15;
 
 resadvflet = loadres('resultsadvflet16.mat');
-resflet = loadres('resultsflet16.mat');
-ressum  = loadres('resultssum16.mat');
-rescrc  = loadres('resultscrc16.mat');
-respcs  = loadres('resultspcs16.mat');
+resflet    = loadres('resultsflet16.mat');
+ressum     = loadres('resultssum16.mat');
+rescrc     = loadres('resultscrc16.mat');
+respcs     = loadres('resultspcs16.mat');
 
 esnodb = -200:1:200;
+
+legtext = '';
 
 nerractual = respcs.nerractual + ressum.nerractual + rescrc.nerractual + resflet.nerractual + resadvflet.nerractual;
 nsim       = respcs.nsim + ressum.nsim + rescrc.nsim + resflet.nsim + resadvflet.nsim;
@@ -18,24 +20,29 @@ nsim       = respcs.nsim + ressum.nsim + rescrc.nsim + resflet.nsim + resadvflet
 figure()
 % Plot undetected error rates
 semilogy(esnodb, nerractual       ./nsim,               '-k' , 'LineWidth',1.25 ), hold on
-semilogy(esnodb, respcs.nerrmiss  ./respcs.nsim,        '-ks', 'LineWidth',1 )
+legtext = [legtext, 'PER'];
+% semilogy(esnodb, respcs.nerrmiss  ./respcs.nsim,        '-ks', 'LineWidth',1 )
+% legtext = [legtext, 'PCS-16'];
 semilogy(esnodb, ressum.nerrmiss  ./ressum.nsim,        '-ko', 'LineWidth',1 )
+legtext = [legtext, 'SUM-16'];
 semilogy(esnodb, rescrc.nerrmiss  ./rescrc.nsim,        '-k*', 'LineWidth',1 )
+legtext = [legtext, 'CRC-16'];
 semilogy(esnodb, resflet.nerrmiss ./resflet.nsim,       '-k^', 'LineWidth',1 )
+legtext = [legtext, 'FLET-16'];
 semilogy(esnodb, resadvflet.nerrmiss ./resadvflet.nsim, '-kv', 'LineWidth',1 )
-
+legtext = [legtext, 'AFLET-16'];
 % Plot false error rates
 % semilogy(esnodb, respcs.nerrfalse  ./respcs.nsim,  ':ks', 'LineWidth',1.5 )
 % semilogy(esnodb, ressum.nerrfalse  ./ressum.nsim,  ':ko', 'LineWidth',1.5 )
 % semilogy(esnodb, rescrc.nerrfalse  ./rescrc.nsim,  ':k^', 'LineWidth',1.5 )
 % semilogy(esnodb, resflet.nerrfalse ./resflet.nsim, ':k*', 'LineWidth',1.5 )
 
-title('Undetected PER')
+title('Undetected PER (128b packets)')
 xlim([5,12]), grid on, grid minor
 legend('PER', 'PCS-16','SUM-16','CRC-16','FS-16')
 
 
-figure()
+% figure()
 % subplot(2,2,1)
 % semilogy(respcs.esnodb, respcs.nerractual./respcs.nsim), hold on
 % semilogy(respcs.esnodb, respcs.NErrCS./respcs.nsim)
@@ -62,14 +69,14 @@ figure()
 % title('CRC-16')
 % xlim([EsN0start,EsN0stop]), grid on, grid minor
 % legend('Actuall Err','CS det Err', 'Missed Err','False Err')
-
-subplot(2,2,4)
-semilogy(resflet.esnodb, resflet.nerractual./resflet.nsim), hold on
-semilogy(resflet.esnodb, resflet.nerrdet./resflet.nsim)
-semilogy(resflet.esnodb, resflet.nerrmiss./resflet.nsim)
-semilogy(resflet.esnodb, resflet.nerrfalse./resflet.nsim), hold off
-title('FS-16')
-xlim([EsN0start,EsN0stop]), grid on, grid minor
-legend('Actuall Err','CS det Err', 'Missed Err','False Err')
+% 
+% subplot(2,2,4)
+% semilogy(resflet.esnodb, resflet.nerractual./resflet.nsim), hold on
+% semilogy(resflet.esnodb, resflet.nerrdet./resflet.nsim)
+% semilogy(resflet.esnodb, resflet.nerrmiss./resflet.nsim)
+% semilogy(resflet.esnodb, resflet.nerrfalse./resflet.nsim), hold off
+% title('FS-16')
+% xlim([EsN0start,EsN0stop]), grid on, grid minor
+% legend('Actuall Err','CS det Err', 'Missed Err','False Err')
 
 end
